e8283d6a0ae3bdf0d4362020c141f474bc0b7d06
sunliang <sunliang711@163.com>
2021-03-03 16:40:54 +0800

    initial commit

---
+++ build.sh
@@ -0,0 +1,246 @@
+#!/bin/bash
+if [ -z "${BASH_SOURCE}" ]; then
+    this=${PWD}
+else
+    rpath="$(readlink ${BASH_SOURCE})"
+    if [ -z "$rpath" ]; then
+        rpath=${BASH_SOURCE}
+    elif echo "$rpath" | grep -q '^/'; then
+        # absolute path
+        echo
+    else
+        # relative path
+        rpath="$(dirname ${BASH_SOURCE})/$rpath"
+    fi
+    this="$(cd $(dirname $rpath) && pwd)"
+fi
+
+export PATH=$PATH:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+user="${SUDO_USER:-$(whoami)}"
+home="$(eval echo ~$user)"
+
+# export TERM=xterm-256color
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+  ncolors=$(tput colors 2>/dev/null)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    CYAN="$(tput setaf 5)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    CYAN=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
+_err(){
+    echo "$*" >&2
+}
+
+_command_exists(){
+    command -v "$@" > /dev/null 2>&1
+}
+
+rootID=0
+
+_runAsRoot(){
+    local trace=0
+    local subshell=0
+    local nostdout=0
+    local nostderr=0
+
+    local optNum=0
+    for opt in ${@};do
+        case "${opt}" in
+            --trace|-x)
+                trace=1
+                ((optNum++))
+                ;;
+            --subshell|-s)
+                subshell=1
+                ((optNum++))
+                ;;
+            --no-stdout)
+                nostdout=1
+                ((optNum++))
+                ;;
+            --no-stderr)
+                nostderr=1
+                ((optNum++))
+                ;;
+            *)
+                break
+                ;;
+        esac
+    done
+
+    shift $(($optNum))
+    local cmd="${*}"
+    bash_c='bash -c'
+    if [ "${EUID}" -ne "${rootID}" ];then
+        if _command_exists sudo; then
+            bash_c='sudo -E bash -c'
+        elif _command_exists su; then
+            bash_c='su -c'
+        else
+            cat >&2 <<-'EOF'
+			Error: this installer needs the ability to run commands as root.
+			We are unable to find either "sudo" or "su" available to make this happen.
+			EOF
+            exit 1
+        fi
+    fi
+
+    local fullcommand="${bash_c} ${cmd}"
+    if [ $nostdout -eq 1 ];then
+        cmd="${cmd} >/dev/null"
+    fi
+    if [ $nostderr -eq 1 ];then
+        cmd="${cmd} 2>/dev/null"
+    fi
+
+    if [ $subshell -eq 1 ];then
+        if [ $trace -eq 1 ];then
+            (set -x; ${bash_c} "${cmd}")
+        else
+            (${bash_c} "${cmd}")
+        fi
+    else
+        if [ $trace -eq 1 ];then
+            set -x; ${bash_c} "${cmd}";set +x;
+        else
+            ${bash_c} "${cmd}"
+        fi
+    fi
+}
+
+function _insert_path(){
+    if [ -z "$1" ];then
+        return
+    fi
+    echo -e ${PATH//:/"\n"} | grep -c "^$1$" >/dev/null 2>&1 || export PATH=$1:$PATH
+}
+
+_run(){
+    local trace=0
+    local subshell=0
+    local nostdout=0
+    local nostderr=0
+
+    local optNum=0
+    for opt in ${@};do
+        case "${opt}" in
+            --trace|-x)
+                trace=1
+                ((optNum++))
+                ;;
+            --subshell|-s)
+                subshell=1
+                ((optNum++))
+                ;;
+            --no-stdout)
+                nostdout=1
+                ((optNum++))
+                ;;
+            --no-stderr)
+                nostderr=1
+                ((optNum++))
+                ;;
+            *)
+                break
+                ;;
+        esac
+    done
+
+    shift $(($optNum))
+    local cmd="${*}"
+    bash_c='bash -c'
+
+    local fullcommand="${bash_c} ${cmd}"
+    if [ $nostdout -eq 1 ];then
+        cmd="${cmd} >/dev/null"
+    fi
+    if [ $nostderr -eq 1 ];then
+        cmd="${cmd} 2>/dev/null"
+    fi
+
+    if [ $subshell -eq 1 ];then
+        if [ $trace -eq 1 ];then
+            (set -x; ${bash_c} "${cmd}")
+        else
+            (${bash_c} "${cmd}")
+        fi
+    else
+        if [ $trace -eq 1 ];then
+            set -x; ${bash_c} "${cmd}";set +x;
+        else
+            ${bash_c} "${cmd}"
+        fi
+    fi
+}
+
+function _root(){
+    if [ ${EUID} -ne ${rootID} ];then
+        echo "Requires root privilege."
+        exit 1
+    fi
+}
+
+ed=vi
+if _command_exists vim; then
+    ed=vim
+fi
+if _command_exists nvim; then
+    ed=nvim
+fi
+# use ENV: editor to override
+if [ -n "${editor}" ];then
+    ed=${editor}
+fi
+###############################################################################
+# write your code below (just define function[s])
+# function is hidden when begin with '_'
+
+src=./ERC1400.sol
+output=output
+
+build(){
+    _run -x solc-0.5.0 --bin $src -o ${output} --overwrite
+}
+
+# write your code above
+###############################################################################
+
+em(){
+    $ed $0
+}
+
+function _help(){
+    cd "${this}"
+    cat<<EOF2
+Usage: $(basename $0) ${bold}CMD${reset}
+
+${bold}CMD${reset}:
+EOF2
+    perl -lne 'print "\t$2" if /^\s*(function)?\s*(\S+)\s*\(\)\s*\{$/' $(basename ${BASH_SOURCE}) | perl -lne "print if /^\t[^_]/"
+}
+
+case "$1" in
+     ""|-h|--help|help)
+        _help
+        ;;
+    *)
+        "$@"
+esac
